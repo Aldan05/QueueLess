9da8531c42b54773029d0742d2981d4f5b7947b6
Aldan05 <aldanaldan837@gmail.com>
2026-08-13 14:47:25 +0530

    Updates

--- test4.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('./backend/routes/auth.js', 'utf8');
-const lines = content.split('\n');
-lines.forEach((line, i) => {
-  if (line.includes('module.exports')) {
-    console.log(`Line ${i + 1}: ${line}`);
-  }
-});
